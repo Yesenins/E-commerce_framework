f3ddc7ff04e37eda1230dc46c7ead036a798e427
serega_svyatskiy <sithserega@gmail.com>
2025-04-05 15:35:20 +0300

    added driver settings

---
+++ src/main/java/drivers/BrowserTypes.java
@@ -0,0 +1,7 @@
+package drivers;
+
+public enum BrowserTypes {
+
+    CHROME,
+    FIREFOX;
+}
---
+++ src/main/java/drivers/WebDriverFactory.java
@@ -0,0 +1,38 @@
+package drivers;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+
+public class WebDriverFactory {
+
+    public static WebDriver createDriver(BrowserTypes browser) {
+        switch(browser) {
+            case CHROME:
+                return createChromeDriver();
+            case FIREFOX:
+                return createFireFoxDriver();
+            default:
+                throw new RuntimeException("Incorrect browser initialization");
+        }
+    }
+
+    private static ChromeDriver createChromeDriver() {
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--window-maximize", "disable-gpu", "--incognito");
+//        options.addArguments("--headless", "disable-gpu", "--incognito");
+        return new ChromeDriver(options);
+    }
+
+    private static FirefoxDriver createFireFoxDriver() {
+        WebDriverManager.firefoxdriver().setup();
+        FirefoxOptions options = new FirefoxOptions();
+        options.addArguments("--window-size=1920,1080", "disable-gpu", "--incognito");
+//        options.addArguments("--headless", "disable-gpu", "--incognito");
+        return new FirefoxDriver(options);
+    }
+}
