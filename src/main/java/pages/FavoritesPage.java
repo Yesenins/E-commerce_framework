d64c5f4fd76c38944d1f318d8c2e1d20b15ff514
serega_svyatskiy <sithserega@gmail.com>
2025-04-16 21:31:09 +0300

    added new FavoritesPage

---
+++ src/main/java/pages/FavoritesPage.java
@@ -0,0 +1,46 @@
+package pages;
+
+import elements.Button;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import utils.ActionUtils;
+import utils.WaitUtils;
+
+import java.util.List;
+
+public class FavoritesPage extends HeaderPage {
+
+    protected final String FAVORITES_PRODUCTS = "//*[contains(@class,'_area_552z7_8')]";
+    protected final String DELETE_FROM_FAVORITES = "//*[contains(@class, 'icon_heart-catalog-added')]";
+
+    public FavoritesPage(WebDriver driver) {
+        super(driver);
+    }
+
+    public FavoritesPage isPageLoaded() {
+        WaitUtils.waitForElementToBeVisible(driver, By.xpath(FAVORITES_PRODUCTS));
+        return this;
+    }
+
+    public int getQuantityOfProducts() {
+        return driver.findElements(By.xpath(FAVORITES_PRODUCTS)).size();
+    }
+
+    public List<WebElement> getProducts() {
+        return driver.findElements(By.xpath(FAVORITES_PRODUCTS));
+    }
+
+    public FavoritesPage deleteFromFavorites() {
+        if(getQuantityOfProducts() == 0) {
+            return this;
+        } else {
+            int quantityOfProducts = getQuantityOfProducts();
+            for (int i =0; i < quantityOfProducts; i++) {
+                ActionUtils.hoverClickElement(By.xpath(FAVORITES_PRODUCTS), By.xpath(DELETE_FROM_FAVORITES), driver);
+                driver.navigate().refresh();
+            }
+            return deleteFromFavorites();
+        }
+    }
+}
