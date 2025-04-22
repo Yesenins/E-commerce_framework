8e283d51372c5ca46809243e3591ceea8b133944
serega_svyatskiy <sithserega@gmail.com>
2025-04-22 18:44:00 +0300

    fix waitUtils

--- src/main/java/utils/WaitUtils.java
+++ src/main/java/utils/WaitUtils.java
@@ -13,28 +13,28 @@ import java.time.Duration;
 public class WaitUtils {
 
     public static void waitForElementToBeClickable(WebDriver driver, By locator) {
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
         wait.until(ExpectedConditions.elementToBeClickable(locator));
     }
 
     public static void waitForElementToBeClickable(WebDriver driver, WebElement element) {
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
         wait.until(ExpectedConditions.elementToBeClickable(element));
     }
 
     public static void waitForElementToBeVisible(WebDriver driver, By locator) {
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
         wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
     }
 
     public static void waitForPageLoaded(WebDriver driver) {
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
         wait.until(webDriver -> ((JavascriptExecutor) webDriver)
                 .executeScript("return document.readyState").equals("complete"));
     }
 
     public static void customWait(WebDriver driver, By locator) {
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
         wait.until(d -> {
             WebElement button = d.findElement(locator);
             return (Boolean) ((JavascriptExecutor) d).executeScript(
